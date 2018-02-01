f9934ae71b5b6718b5d2423b7c30cd0134edc89c
AussieGuy0 <anthony.bruno196@gmail.com>
2018-02-01 20:39:39 +1030

    Basic logic for generating values for different input types

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -21,6 +21,41 @@ function main() {
 }
 
 function fillInput(inputElement) {
-    inputElement.value = "aa";
-
+    const type = inputElement.type;
+    if (type != null) {
+        if (type === 'email') {
+            inputElement.value = generateEmail();
+        } else if (type === 'number') {
+            inputElement.value = generateNumber();
+        } else {
+            inputElement.value = generateRandomWord();
+        }
+    } else {
+        inputElement.value = generateRandomWord();
+    }
 }
+
+function generateEmail() {
+    return generateRandomWord() + '@' + generateDomain();
+}
+
+function generateNumber() {
+    return Math.floor(Math.random() * 100);
+}
+
+const domains = ["gmail.com", "yahoo.com", "hotmail.com"];
+
+function generateDomain() {
+    return getRandomElementFromArray(domains);
+}
+
+const militaryAlphabet = ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf']; //TODO
+function generateRandomWord() {
+    return getRandomElementFromArray(militaryAlphabet);
+}
+
+function getRandomElementFromArray(arr) {
+    const index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
+
